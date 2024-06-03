d7f074ee09d18a6a45130a0600d24f196537e385
Martin Nuñez Igarzabal <martin.mr1367@gmail.com>
2024-06-03 19:08:58 -0300

    Update
    Update

--- layers/layers.js
+++ layers/layers.js
@@ -38,96 +38,83 @@ var wms_layers = [];
                 url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
             })
         });
-var lyr_sugarcane_3 = new ol.layer.Image({
-                            opacity: 1,
-                            title: "sugarcane",
-                            
-                            
-                            source: new ol.source.ImageStatic({
-                               url: "./layers/sugarcane_3.png",
-    attributions: ' ',
-                                projection: 'EPSG:4326',
-                                alwaysInRange: true,
-                                imageExtent: [58.832378, -19.249563, 142.998708, 43.750185]
-                            })
-                        });
-var format_Sugarcane_4 = new ol.format.GeoJSON();
-var features_Sugarcane_4 = format_Sugarcane_4.readFeatures(json_Sugarcane_4, 
+var format_Sugarcane_3 = new ol.format.GeoJSON();
+var features_Sugarcane_3 = format_Sugarcane_3.readFeatures(json_Sugarcane_3, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
-var jsonSource_Sugarcane_4 = new ol.source.Vector({
+var jsonSource_Sugarcane_3 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_Sugarcane_4.addFeatures(features_Sugarcane_4);
-var lyr_Sugarcane_4 = new ol.layer.Vector({
+jsonSource_Sugarcane_3.addFeatures(features_Sugarcane_3);
+var lyr_Sugarcane_3 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_Sugarcane_4, 
-                style: style_Sugarcane_4,
+                source:jsonSource_Sugarcane_3, 
+                style: style_Sugarcane_3,
                 popuplayertitle: "Sugarcane ",
-                interactive: true,
+                interactive: false,
     title: 'Sugarcane <br />\
-    <img src="styles/legend/Sugarcane_4_0.png" /> 10 - 75 kTons<br />\
-    <img src="styles/legend/Sugarcane_4_1.png" /> 75 - 80 kTons<br />\
-    <img src="styles/legend/Sugarcane_4_2.png" /> 80 - 100 kTons<br />\
-    <img src="styles/legend/Sugarcane_4_3.png" /> 100 - 125 kTons<br />\
-    <img src="styles/legend/Sugarcane_4_4.png" /> 125 - 150 kTons<br />\
-    <img src="styles/legend/Sugarcane_4_5.png" /> 150 - 300 kTons<br />\
-    <img src="styles/legend/Sugarcane_4_6.png" /> 300 - 500 kTons<br />\
-    <img src="styles/legend/Sugarcane_4_7.png" /> > 500 kTons<br />'
+    <img src="styles/legend/Sugarcane_3_0.png" /> 10 - 75 kTons<br />\
+    <img src="styles/legend/Sugarcane_3_1.png" /> 75 - 80 kTons<br />\
+    <img src="styles/legend/Sugarcane_3_2.png" /> 80 - 100 kTons<br />\
+    <img src="styles/legend/Sugarcane_3_3.png" /> 100 - 125 kTons<br />\
+    <img src="styles/legend/Sugarcane_3_4.png" /> 125 - 150 kTons<br />\
+    <img src="styles/legend/Sugarcane_3_5.png" /> 150 - 300 kTons<br />\
+    <img src="styles/legend/Sugarcane_3_6.png" /> 300 - 500 kTons<br />\
+    <img src="styles/legend/Sugarcane_3_7.png" /> > 500 kTons<br />'
         });
-var format_IndiaBoundary_5 = new ol.format.GeoJSON();
-var features_IndiaBoundary_5 = format_IndiaBoundary_5.readFeatures(json_IndiaBoundary_5, 
+var format_IndiaBoundary_4 = new ol.format.GeoJSON();
+var features_IndiaBoundary_4 = format_IndiaBoundary_4.readFeatures(json_IndiaBoundary_4, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
-var jsonSource_IndiaBoundary_5 = new ol.source.Vector({
+var jsonSource_IndiaBoundary_4 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_IndiaBoundary_5.addFeatures(features_IndiaBoundary_5);
-var lyr_IndiaBoundary_5 = new ol.layer.Vector({
+jsonSource_IndiaBoundary_4.addFeatures(features_IndiaBoundary_4);
+var lyr_IndiaBoundary_4 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_IndiaBoundary_5, 
-                style: style_IndiaBoundary_5,
+                source:jsonSource_IndiaBoundary_4, 
+                style: style_IndiaBoundary_4,
                 popuplayertitle: "India Boundary",
                 interactive: false,
-                    title: '<img src="styles/legend/IndiaBoundary_5.png" /> India Boundary'
+                    title: '<img src="styles/legend/IndiaBoundary_4.png" /> India Boundary'
                 });
-var format_PaperMills_6 = new ol.format.GeoJSON();
-var features_PaperMills_6 = format_PaperMills_6.readFeatures(json_PaperMills_6, 
+var format_PaperMills_5 = new ol.format.GeoJSON();
+var features_PaperMills_5 = format_PaperMills_5.readFeatures(json_PaperMills_5, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
-var jsonSource_PaperMills_6 = new ol.source.Vector({
+var jsonSource_PaperMills_5 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_PaperMills_6.addFeatures(features_PaperMills_6);
-var lyr_PaperMills_6 = new ol.layer.Vector({
+jsonSource_PaperMills_5.addFeatures(features_PaperMills_5);
+var lyr_PaperMills_5 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_PaperMills_6, 
-                style: style_PaperMills_6,
+                source:jsonSource_PaperMills_5, 
+                style: style_PaperMills_5,
                 popuplayertitle: "Paper Mills",
                 interactive: true,
-                    title: '<img src="styles/legend/PaperMills_6.png" /> Paper Mills'
+                    title: '<img src="styles/legend/PaperMills_5.png" /> Paper Mills'
                 });
-var format_PaperMillssugarcaneTOP20_7 = new ol.format.GeoJSON();
-var features_PaperMillssugarcaneTOP20_7 = format_PaperMillssugarcaneTOP20_7.readFeatures(json_PaperMillssugarcaneTOP20_7, 
+var format_PaperMillssugarcaneTOP20_6 = new ol.format.GeoJSON();
+var features_PaperMillssugarcaneTOP20_6 = format_PaperMillssugarcaneTOP20_6.readFeatures(json_PaperMillssugarcaneTOP20_6, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4326'});
-var jsonSource_PaperMillssugarcaneTOP20_7 = new ol.source.Vector({
+var jsonSource_PaperMillssugarcaneTOP20_6 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_PaperMillssugarcaneTOP20_7.addFeatures(features_PaperMillssugarcaneTOP20_7);
-var lyr_PaperMillssugarcaneTOP20_7 = new ol.layer.Vector({
+jsonSource_PaperMillssugarcaneTOP20_6.addFeatures(features_PaperMillssugarcaneTOP20_6);
+var lyr_PaperMillssugarcaneTOP20_6 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_PaperMillssugarcaneTOP20_7, 
-                style: style_PaperMillssugarcaneTOP20_7,
+                source:jsonSource_PaperMillssugarcaneTOP20_6, 
+                style: style_PaperMillssugarcaneTOP20_6,
                 popuplayertitle: "Paper Mills sugarcane TOP 20",
                 interactive: true,
     title: 'Paper Mills sugarcane TOP 20<br />\
-    <img src="styles/legend/PaperMillssugarcaneTOP20_7_0.png" /> 101231 - 155423 kTons<br />\
-    <img src="styles/legend/PaperMillssugarcaneTOP20_7_1.png" /> 155423 - 184938 kTons<br />\
-    <img src="styles/legend/PaperMillssugarcaneTOP20_7_2.png" /> 184938 - 202601 kTons<br />'
+    <img src="styles/legend/PaperMillssugarcaneTOP20_6_0.png" /> 101231 - 155423 kTons<br />\
+    <img src="styles/legend/PaperMillssugarcaneTOP20_6_1.png" /> 155423 - 184938 kTons<br />\
+    <img src="styles/legend/PaperMillssugarcaneTOP20_6_2.png" /> 184938 - 202601 kTons<br />'
         });
 var group_PaperMills = new ol.layer.Group({
-                                layers: [lyr_PaperMills_6,lyr_PaperMillssugarcaneTOP20_7,],
+                                layers: [lyr_PaperMills_5,lyr_PaperMillssugarcaneTOP20_6,],
                                 fold: "open",
                                 title: "Paper Mills"});
 var group_Feedstock = new ol.layer.Group({
-                                layers: [lyr_sugarcane_3,lyr_Sugarcane_4,],
+                                layers: [lyr_Sugarcane_3,],
                                 fold: "open",
                                 title: "Feedstock"});
 var group_Basemap = new ol.layer.Group({
@@ -135,20 +122,20 @@ var group_Basemap = new ol.layer.Group({
                                 fold: "open",
                                 title: "Base map "});
 
-lyr_ESRIPhysical_0.setVisible(true);lyr_BaseMapGrey_1.setVisible(true);lyr_GoogleSatellite_2.setVisible(true);lyr_sugarcane_3.setVisible(true);lyr_Sugarcane_4.setVisible(true);lyr_IndiaBoundary_5.setVisible(true);lyr_PaperMills_6.setVisible(true);lyr_PaperMillssugarcaneTOP20_7.setVisible(true);
-var layersList = [group_Basemap,group_Feedstock,lyr_IndiaBoundary_5,group_PaperMills];
-lyr_Sugarcane_4.set('fieldAliases', {'Sugarcane': 'Sugarcane', });
-lyr_IndiaBoundary_5.set('fieldAliases', {'fid_1': 'fid_1', 'GID_0': 'GID_0', 'NAME_0': 'NAME_0', });
-lyr_PaperMills_6.set('fieldAliases', {'Site Code': 'Site Code', 'Company Na': 'Company Na', 'Site Name': 'Site Name', 'Site City': 'Site City', 'Site State': 'Site State', 'Site Count': 'Site Count', 'My Site In': 'My Site In', 'My Site _1': 'My Site _1', 'Sugarcane': 'Sugarcane', });
-lyr_PaperMillssugarcaneTOP20_7.set('fieldAliases', {'Site Code': 'Site Code', 'Company Na': 'Company Na', 'Site Name': 'Site Name', 'Site City': 'Site City', 'Site State': 'Site State', 'Site Count': 'Site Count', 'My Site In': 'My Site In', 'My Site _1': 'My Site _1', 'Sugarcane': 'Sugarcane', 'N° TOP 20': 'N° TOP 20', });
-lyr_Sugarcane_4.set('fieldImages', {'Sugarcane': 'TextEdit', });
-lyr_IndiaBoundary_5.set('fieldImages', {'fid_1': 'TextEdit', 'GID_0': 'TextEdit', 'NAME_0': 'TextEdit', });
-lyr_PaperMills_6.set('fieldImages', {'Site Code': 'TextEdit', 'Company Na': 'TextEdit', 'Site Name': 'TextEdit', 'Site City': 'TextEdit', 'Site State': 'TextEdit', 'Site Count': 'TextEdit', 'My Site In': 'TextEdit', 'My Site _1': 'TextEdit', 'Sugarcane': 'TextEdit', });
-lyr_PaperMillssugarcaneTOP20_7.set('fieldImages', {'Site Code': 'TextEdit', 'Company Na': 'TextEdit', 'Site Name': 'TextEdit', 'Site City': 'TextEdit', 'Site State': 'TextEdit', 'Site Count': 'TextEdit', 'My Site In': 'TextEdit', 'My Site _1': 'TextEdit', 'Sugarcane': 'TextEdit', 'N° TOP 20': 'TextEdit', });
-lyr_Sugarcane_4.set('fieldLabels', {'Sugarcane': 'inline label - always visible', });
-lyr_IndiaBoundary_5.set('fieldLabels', {'fid_1': 'no label', 'GID_0': 'no label', 'NAME_0': 'no label', });
-lyr_PaperMills_6.set('fieldLabels', {'Site Code': 'inline label - always visible', 'Company Na': 'inline label - always visible', 'Site Name': 'inline label - always visible', 'Site City': 'inline label - always visible', 'Site State': 'inline label - always visible', 'Site Count': 'inline label - always visible', 'My Site In': 'hidden field', 'My Site _1': 'hidden field', 'Sugarcane': 'inline label - always visible', });
-lyr_PaperMillssugarcaneTOP20_7.set('fieldLabels', {'Site Code': 'inline label - always visible', 'Company Na': 'inline label - always visible', 'Site Name': 'inline label - always visible', 'Site City': 'inline label - always visible', 'Site State': 'inline label - always visible', 'Site Count': 'inline label - always visible', 'My Site In': 'hidden field', 'My Site _1': 'hidden field', 'Sugarcane': 'inline label - always visible', 'N° TOP 20': 'inline label - always visible', });
-lyr_PaperMillssugarcaneTOP20_7.on('precompose', function(evt) {
+lyr_ESRIPhysical_0.setVisible(true);lyr_BaseMapGrey_1.setVisible(true);lyr_GoogleSatellite_2.setVisible(true);lyr_Sugarcane_3.setVisible(true);lyr_IndiaBoundary_4.setVisible(true);lyr_PaperMills_5.setVisible(true);lyr_PaperMillssugarcaneTOP20_6.setVisible(true);
+var layersList = [group_Basemap,group_Feedstock,lyr_IndiaBoundary_4,group_PaperMills];
+lyr_Sugarcane_3.set('fieldAliases', {'Sugarcane': 'Sugarcane', });
+lyr_IndiaBoundary_4.set('fieldAliases', {'fid_1': 'fid_1', 'GID_0': 'GID_0', 'NAME_0': 'NAME_0', });
+lyr_PaperMills_5.set('fieldAliases', {'Site Code': 'Site Code', 'Company Na': 'Company Na', 'Site Name': 'Site Name', 'Site City': 'Site City', 'Site State': 'Site State', 'Site Count': 'Site Count', 'My Site In': 'My Site In', 'My Site _1': 'My Site _1', 'Sugarcane': 'Sugarcane', });
+lyr_PaperMillssugarcaneTOP20_6.set('fieldAliases', {'Site Code': 'Site Code', 'Company Na': 'Company Na', 'Site Name': 'Site Name', 'Site City': 'Site City', 'Site State': 'Site State', 'Site Count': 'Site Count', 'My Site In': 'My Site In', 'My Site _1': 'My Site _1', 'Sugarcane': 'Sugarcane', 'N° TOP 20': 'N° TOP 20', });
+lyr_Sugarcane_3.set('fieldImages', {'Sugarcane': 'TextEdit', });
+lyr_IndiaBoundary_4.set('fieldImages', {'fid_1': 'TextEdit', 'GID_0': 'TextEdit', 'NAME_0': 'TextEdit', });
+lyr_PaperMills_5.set('fieldImages', {'Site Code': 'TextEdit', 'Company Na': 'TextEdit', 'Site Name': 'TextEdit', 'Site City': 'TextEdit', 'Site State': 'TextEdit', 'Site Count': 'TextEdit', 'My Site In': 'TextEdit', 'My Site _1': 'TextEdit', 'Sugarcane': 'TextEdit', });
+lyr_PaperMillssugarcaneTOP20_6.set('fieldImages', {'Site Code': 'TextEdit', 'Company Na': 'TextEdit', 'Site Name': 'TextEdit', 'Site City': 'TextEdit', 'Site State': 'TextEdit', 'Site Count': 'TextEdit', 'My Site In': 'TextEdit', 'My Site _1': 'TextEdit', 'Sugarcane': 'TextEdit', 'N° TOP 20': 'TextEdit', });
+lyr_Sugarcane_3.set('fieldLabels', {'Sugarcane': 'inline label - always visible', });
+lyr_IndiaBoundary_4.set('fieldLabels', {'fid_1': 'no label', 'GID_0': 'no label', 'NAME_0': 'no label', });
+lyr_PaperMills_5.set('fieldLabels', {'Site Code': 'inline label - always visible', 'Company Na': 'inline label - always visible', 'Site Name': 'inline label - always visible', 'Site City': 'inline label - always visible', 'Site State': 'inline label - always visible', 'Site Count': 'inline label - always visible', 'My Site In': 'hidden field', 'My Site _1': 'hidden field', 'Sugarcane': 'inline label - always visible', });
+lyr_PaperMillssugarcaneTOP20_6.set('fieldLabels', {'Site Code': 'inline label - always visible', 'Company Na': 'inline label - always visible', 'Site Name': 'inline label - always visible', 'Site City': 'inline label - always visible', 'Site State': 'inline label - always visible', 'Site Count': 'inline label - always visible', 'My Site In': 'hidden field', 'My Site _1': 'hidden field', 'Sugarcane': 'inline label - always visible', 'N° TOP 20': 'inline label - always visible', });
+lyr_PaperMillssugarcaneTOP20_6.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
---
+++ styles/PaperMillssugarcaneTOP20_6_style.js
@@ -0,0 +1,50 @@
+var size = 0;
+var placement = 'point';
+
+var style_PaperMillssugarcaneTOP20_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("Sugarcane");
+    var labelText = "";
+    size = 0;
+    var labelFont = "11.700000000000001px \'Open Sans\', sans-serif";
+    var labelFill = "#ffffff";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 101231.000000 && value <= 155423.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 5.2 + size, points: 3,
+            stroke: new ol.style.Stroke({color: 'rgba(201,197,169,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(43,131,76,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 155423.000000 && value <= 184937.666667) {
+            style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 5.2 + size, points: 3,
+            stroke: new ol.style.Stroke({color: 'rgba(201,197,169,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(255,255,191,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 184937.666667 && value <= 202601.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 5.2 + size, points: 3,
+            stroke: new ol.style.Stroke({color: 'rgba(201,197,169,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(215,25,28,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
